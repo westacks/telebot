1312725c45f8cbd2ebdc9145aca0736f77a0307f
PunyFlash <puny.flash@gmail.com>
2020-08-25 03:06:12 +0300

    Add payments objects

---
+++ src/Objects/Payments/ShippingAddress.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WeStacks\TeleBot\Objects\Payments;
+
+use WeStacks\TeleBot\Objects\TelegramObject;
+/**
+ * This object represents a shipping address.
+ * 
+ * @property String                    $country_code             ISO 3166-1 alpha-2 country code
+ * @property String                    $state                    State, if applicable
+ * @property String                    $city                     City
+ * @property String                    $street_line1             First line for the address
+ * @property String                    $street_line2             Second line for the address
+ * @property String                    $post_code                Address post code
+ * 
+ * @package WeStacks\TeleBot\Objects\Payments
+ */
+class ShippingAddress extends TelegramObject
+{
+    protected function relations()
+    {
+        return [
+            'country_code'      => 'string',
+            'state'             => 'string',
+            'city'              => 'string',
+            'street_line1'      => 'string',
+            'street_line2'      => 'string',
+            'post_code'         => 'string'
+        ];
+    }
+}
---
+++ src/Objects/Payments/SuccessfulPayment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WeStacks\TeleBot\Objects\Payments;
+
+use WeStacks\TeleBot\Objects\TelegramObject;
+
+/**
+ * This object contains basic information about a successful payment.
+ * 
+ * @property String                    $currency                            Three-letter ISO 4217 currency code
+ * @property Integer                   $total_amount                        Total price in the smallest units of the currency (integer, not float/double). For example, for a price of US$ 1.45 pass amount = 145. See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
+ * @property String                    $invoice_payload                     Bot specified invoice payload
+ * @property String                    $shipping_option_id                  Optional. Identifier of the shipping option chosen by the user
+ * @property OrderInfo                 $order_info                          Optional. Order info provided by the user
+ * @property String                    $telegram_payment_charge_id          Telegram payment identifier
+ * @property String                    $provider_payment_charge_id          Provider payment identifier
+ * 
+ * @package WeStacks\TeleBot\Objects\Payments
+ */
+class SuccessfulPayment extends TelegramObject
+{
+    protected function relations()
+    {
+        return [
+            'currency'                      => 'string',
+            'total_amount'                  => 'integer',
+            'invoice_payload'               => 'string',
+            'shipping_option_id'            => 'string',
+            'order_info'                    => OrderInfo::class,
+            'telegram_payment_charge_id'    => 'string',
+            'provider_payment_charge_id'    => 'string'
+        ];
+    }
+}
