70afdd98753daef5771d46d9d37e2fd50eab0c45
PunyFlash <puny.flash@gmail.com>
2020-09-08 15:02:49 +0300

    Fix webhook test

--- tests/Feature/WebhookTest.php
+++ tests/Feature/WebhookTest.php
@@ -18,32 +18,23 @@ class WebhookTest extends TestCase
         $this->bot = new Bot(getenv('TELEGRAM_BOT_TOKEN'));
     }
 
-    public function testWebhookEmpty()
+    public function testWebhook()
     {
+        // Check empty webhook
         $info = $this->bot->getWebhookInfo();
-
         $this->assertInstanceOf(WebhookInfo::class, $info);
         $this->assertEquals('', $info->url);
-    }
 
-    public function testSetWebhook()
-    {
-        $res = $this->bot->setWebhook([
-            'url' => 'https://www.example.com/telegram_webhook'
-        ]);
+        // Set webhook
+        $res = $this->bot->setWebhook([ 'url' => 'https://www.example.com/telegram_webhook' ]);
         $this->assertTrue($res);
-    }
 
-    public function testWebhookSetup()
-    {
+        // Check set webhook
         $info = $this->bot->getWebhookInfo();
-
         $this->assertInstanceOf(WebhookInfo::class, $info);
         $this->assertEquals('https://www.example.com/telegram_webhook', $info->url);
-    }
 
-    public function testWebhookRemove()
-    {
+        // Remove webhook
         $res = $this->bot->deleteWebhook();
         $this->assertTrue($res);
     }
