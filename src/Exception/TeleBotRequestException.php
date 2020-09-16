56ffe04d56ce157de84e0f5aa687926565be39cb
PunyFlash <puny.flash@gmail.com>
2020-09-16 23:08:45 +0300

    Use response parameters for request error

--- src/Exception/TeleBotRequestException.php
+++ src/Exception/TeleBotRequestException.php
@@ -2,11 +2,19 @@
 
 namespace WeStacks\TeleBot\Exception;
 
+use WeStacks\TeleBot\Objects\ResponseParameters;
+
 class TeleBotRequestException extends TeleBotException
 {
     public static function requestError($result)
     {
-        // $parameters = ResponseParameters::create($result->parameters ?? null);
-        return new TeleBotRequestException($result->description, $result->error_code);
+        $text = $result->description;
+        if (isset($result->parameters))
+        {
+            $parameters = ResponseParameters::create($result->parameters ?? null);
+            $text .= '; Parameters: '.$parameters;
+        }
+
+        return new TeleBotRequestException($text, $result->error_code);
     }
 }
