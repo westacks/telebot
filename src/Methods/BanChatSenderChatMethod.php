615347c876eb1381c9728b5d65afa1ae8189b5f4
Dmytro <dmytro.morozov@aniart.com.ua>
2021-12-09 12:10:22 +0200

    Add ban/unban chat methods

---
+++ src/Methods/BanChatSenderChatMethod.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WeStacks\TeleBot\Methods;
+
+use WeStacks\TeleBot\Helpers\TypeCaster;
+use WeStacks\TeleBot\Interfaces\TelegramMethod;
+
+class BanChatSenderChatMethod extends TelegramMethod
+{
+    protected function request()
+    {
+        return [
+            'type' => 'POST',
+            'url' => "{$this->api}/bot{$this->token}/banChatSenderChat",
+            'send' => $this->send(),
+            'expect' => 'boolean',
+        ];
+    }
+
+    private function send()
+    {
+        $parameters = [
+            'chat_id' => 'string',
+            'sender_chat_id' => 'integer',
+        ];
+
+        $object = TypeCaster::castValues($this->arguments[0] ?? [], $parameters);
+
+        return ['json' => TypeCaster::stripArrays($object)];
+    }
+}
