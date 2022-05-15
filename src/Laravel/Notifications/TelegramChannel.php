cd5103990d85f330ca142205d565a970409268c5
PunyFlash <puny.flash@gmail.com>
2022-05-15 22:01:23 +0300

    Emmit notification events

--- src/Laravel/Notifications/TelegramChannel.php
+++ src/Laravel/Notifications/TelegramChannel.php
@@ -4,8 +4,11 @@ namespace WeStacks\TeleBot\Laravel\Notifications;
 
 use Exception;
 use GuzzleHttp\Promise\Utils;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Notifications\Events\NotificationFailed;
+use Illuminate\Notifications\Events\NotificationSending;
+use Illuminate\Notifications\Events\NotificationSent;
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Log;
 use WeStacks\TeleBot\BotManager;
 
 class TelegramChannel
@@ -15,9 +18,15 @@ class TelegramChannel
      */
     protected $botmanager;
 
-    public function __construct(BotManager $botmanager)
+    /**
+     * @var Dispatcher
+     */
+    private $dispatcher;
+
+    public function __construct(BotManager $botmanager, Dispatcher $dispatcher)
     {
         $this->botmanager = $botmanager;
+        $this->dispatcher = $dispatcher;
     }
 
     /**
@@ -29,25 +38,31 @@ class TelegramChannel
      */
     public function send($notifiable, Notification $notification)
     {
-        if (! method_exists($notification, 'toTelegram')) {
-            return null;
-        }
-
         $data = call_user_func([$notification, 'toTelegram'], $notifiable);
         $data = new TelegramNotification((string) $data);
         $data = $data->jsonSerialize();
         $bot = $this->botmanager->bot($data['bot'] ?? null);
 
+        $this->dispatcher->dispatch(new NotificationSending($notifiable, $notification, static::class));
+
         $promises = [];
+        $errors = [];
 
         foreach (($data['actions'] ?? []) as $action) {
             $promises[] = $bot->async()->exceptions()
                 ->{$action['method']}($action['arguments'])
-                ->otherwise(function (Exception $e) {
-                    Log::error("Error while sending notification to Telegram: {$e->getMessage()}");
+                ->otherwise(function (Exception $exception) use (&$errors) {
+                    $errors[] = $exception;
+                    return $exception;
                 });
         }
 
-        return Utils::unwrap($promises);
+        $results = Utils::unwrap($promises);
+        $report = count($errors) ?
+            new NotificationFailed($notifiable, $notification, static::class, $results) :
+            new NotificationSent($notifiable, $notification, static::class, $results);
+
+        $this->dispatcher->dispatch($report);
+        return $results;
     }
 }
