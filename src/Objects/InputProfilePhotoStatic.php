14334892eae161d3c2d4963d98d15fe639dd766d
punyflash <14828695+punyflash@users.noreply.github.com>
2025-04-14 01:49:17 +0000

    [auto] update telegram bot API

--- src/Objects/InputProfilePhotoStatic.php
+++ src/Objects/InputProfilePhotoStatic.php
@@ -4,7 +4,7 @@ namespace WeStacks\TeleBot\Objects;
 
 /**
  * A static profile photo in the .JPG format.
- * @property-read string $type Type of the profile photo, must be “static”
+ * @property-read string $type Type of the profile photo, must be static
  * @property-read string $photo The static profile photo. Profile photos can't be reused and can only be uploaded as a new file, so you can pass “attach://<file_attach_name>” if the photo was uploaded using multipart/form-data under <file_attach_name>. More information on Sending Files »
  *
  * @see https://core.telegram.org/bots/api#inputprofilephotostatic
