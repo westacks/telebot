c515532b77796444e71d3972dc6991829d4a64ee
punyflash <14828695+punyflash@users.noreply.github.com>
2025-04-15 01:34:06 +0000

    [auto] update telegram bot API

--- src/Objects/InputProfilePhotoAnimated.php
+++ src/Objects/InputProfilePhotoAnimated.php
@@ -4,7 +4,7 @@ namespace WeStacks\TeleBot\Objects;
 
 /**
  * An animated profile photo in the MPEG4 format.
- * @property-read string $type Type of the profile photo, must be “animated”
+ * @property-read string $type Type of the profile photo, must be animated
  * @property-read string $animation The animated profile photo. Profile photos can't be reused and can only be uploaded as a new file, so you can pass “attach://<file_attach_name>” if the photo was uploaded using multipart/form-data under <file_attach_name>. More information on Sending Files »
  * @property-read ?float $main_frame_timestamp Optional. Timestamp in seconds of the frame that will be used as the static profile photo. Defaults to 0.0.
  *
--- src/Objects/InputStoryContentVideo.php
+++ src/Objects/InputStoryContentVideo.php
@@ -4,7 +4,7 @@ namespace WeStacks\TeleBot\Objects;
 
 /**
  * Describes a video to post as a story.
- * @property-read string $type Type of the content, must be “video”
+ * @property-read string $type Type of the content, must be video
  * @property-read string $video The video to post as a story. The video must be of the size 720x1280, streamable, encoded with H.265 codec, with key frames added each second in the MPEG4 format, and must not exceed 30 MB. The video can't be reused and can only be uploaded as a new file, so you can pass “attach://<file_attach_name>” if the video was uploaded using multipart/form-data under <file_attach_name>. More information on Sending Files »
  * @property-read ?float $duration Optional. Precise duration of the video in seconds; 0-60
  * @property-read ?float $cover_frame_timestamp Optional. Timestamp in seconds of the frame that will be used as the static cover for the story. Defaults to 0.0.
