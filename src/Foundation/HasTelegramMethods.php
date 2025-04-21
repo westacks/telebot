d5679684197729bd344cbfdde291b7d4b5f513e9
punyflash <14828695+punyflash@users.noreply.github.com>
2025-04-21 01:06:34 +0000

    [auto] update telegram bot API

--- src/Foundation/HasTelegramMethods.php
+++ src/Foundation/HasTelegramMethods.php
@@ -1276,7 +1276,7 @@ use WeStacks\TeleBot\Objects\WebhookInfo;
  * - _int_ `$message_id` __Required: Yes__. Unique identifier of the message to mark as read
  *
  *
- * @method PromiseInterface|true deleteBusinessMessages(...$parameters) Delete messages on behalf of a business account. Requires the can_delete_outgoing_messages business bot right to delete messages sent by the bot itself, or the can_delete_all_messages business bot right to delete any message. Returns True on success.
+ * @method PromiseInterface|true deleteBusinessMessages(...$parameters) Delete messages on behalf of a business account. Requires the can_delete_sent_messages business bot right to delete messages sent by the bot itself, or the can_delete_all_messages business bot right to delete any message. Returns True on success.
  *
  * {@see https://core.telegram.org/bots/api#deletebusinessmessages}
  *
--- src/Methods/DeleteBusinessMessagesMethod.php
+++ src/Methods/DeleteBusinessMessagesMethod.php
@@ -5,7 +5,7 @@ namespace WeStacks\TeleBot\Methods;
 use WeStacks\TeleBot\Foundation\TelegramMethod;
 
 /**
- * Delete messages on behalf of a business account. Requires the can_delete_outgoing_messages business bot right to delete messages sent by the bot itself, or the can_delete_all_messages business bot right to delete any message. Returns True on success.
+ * Delete messages on behalf of a business account. Requires the can_delete_sent_messages business bot right to delete messages sent by the bot itself, or the can_delete_all_messages business bot right to delete any message. Returns True on success.
  *
  * @property-read string $business_connection_id Unique identifier of the business connection on behalf of which to delete the messages
  * @property-read int[] $message_ids A JSON-serialized list of 1-100 identifiers of messages to delete. All messages must be from the same chat. See deleteMessage for limitations on which messages can be deleted
