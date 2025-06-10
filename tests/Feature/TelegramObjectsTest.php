3130029f8ecdccc42c4f08c6af6ff0128f9635b0
Dmytro Morozov <puny.flash@gmail.com>
2025-06-10 21:27:06 +0300

    Test photo and media group

--- tests/Feature/TelegramObjectsTest.php
+++ tests/Feature/TelegramObjectsTest.php
@@ -1,7 +1,11 @@
 <?php
 
+use WeStacks\TeleBot\Methods\SendMediaGroupMethod;
 use WeStacks\TeleBot\Methods\SendMessageMethod;
+use WeStacks\TeleBot\Methods\SendPhotoMethod;
 use WeStacks\TeleBot\Objects\InlineKeyboardMarkup;
+use WeStacks\TeleBot\Objects\InputFile;
+use WeStacks\TeleBot\Objects\InputMediaPhoto;
 use WeStacks\TeleBot\Objects\ReplyKeyboardMarkup;
 use WeStacks\TeleBot\Objects\Update;
 
@@ -54,3 +58,36 @@ test('plain keyboard', function () {
         ]
     ], SendMessageMethod::class);
 })->throws(\InvalidArgumentException::class);
+
+test('send photo test', function () {
+    $method = synthesize([
+        'chat_id' => 1,
+        'photo' => 'AgACAgIAAxkBAAIyJ2Mx5uBv0nL2hUv3m4nWk2kF8gACZQACFwEAAu8t6QI8ZpI6UvIb8gQ',
+    ], SendPhotoMethod::class);
+
+    expect($method)->toBeInstanceOf(SendPhotoMethod::class);
+    expect($method->photo)->toBeString();
+
+    $method = synthesize([
+        'chat_id' => 1,
+        'photo' => __DIR__.'/../Lib/test-document.txt',
+    ], SendPhotoMethod::class);
+
+    expect($method)->toBeInstanceOf(SendPhotoMethod::class);
+    expect($method->photo)->toBeInstanceOf(InputFile::class);
+});
+
+test('send media group', function () {
+    $method = synthesize([
+        'chat_id' => 1,
+        'media' => [
+            ['type' => 'photo', 'media' => 'AgACAgIAAxkBAAIyJ2Mx5uBv0nL2hUv3m4nWk2kF8gACZQACFwEAAu8t6QI8ZpI6UvIb8gQ'],
+            ['type' => 'photo', 'media' => __DIR__.'/../Lib/test-document.txt'],
+        ],
+    ], SendMediaGroupMethod::class);
+
+    expect($method)->toBeInstanceOf(SendMediaGroupMethod::class);
+    expect($method->media[0])->and($method->media[1])->toBeInstanceOf(InputMediaPhoto::class);
+    expect($method->media[0]->media)->toBeString();
+    expect($method->media[1]->media)->toBeInstanceOf(InputFile::class);
+});
