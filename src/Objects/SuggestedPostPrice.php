5cdbb999234c20cfec153552e607f5b567140500
punyflash <14828695+punyflash@users.noreply.github.com>
2025-08-18 01:13:41 +0000

    [auto] update telegram bot API

--- src/Objects/SuggestedPostPrice.php
+++ src/Objects/SuggestedPostPrice.php
@@ -5,7 +5,7 @@ namespace WeStacks\TeleBot\Objects;
 use WeStacks\TeleBot\Foundation\TelegramObject;
 
 /**
- * Desribes price of a suggested post.
+ * Describes the price of a suggested post.
  * @property-read string $currency Currency in which the post will be paid. Currently, must be one of “XTR” for Telegram Stars or “TON” for toncoins
  * @property-read int $amount The amount of the currency that will be paid for the post in the smallest units of the currency, i.e. Telegram Stars or nanotoncoins. Currently, price in Telegram Stars must be between 5 and 100000, and price in nanotoncoins must be between 10000000 and 10000000000000.
  *
