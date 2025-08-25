817e3dd25f47543289dd54bbc7836f43dd33ca97
John Ber <32464352+JhnBer@users.noreply.github.com>
2025-08-25 12:16:25 +0200

    Refactor RequestInputHandler to use Chat ID (#132)
    * RequestInputHandler.php patch
    - support request by User|Chat $target, and additional User and thread_id params
    
    * UpdateHandlingTest.php upd
    
    * PSR style fix

--- tests/Feature/UpdateHandlingTest.php
+++ tests/Feature/UpdateHandlingTest.php
@@ -6,7 +6,7 @@ use WeStacks\TeleBot\Foundation\TeleBotResponse;
 use WeStacks\TeleBot\Objects;
 use WeStacks\TeleBot\TeleBot;
 
-test('request input handler', function (TeleBot $bot) {
+test('request input handler by user', function (TeleBot $bot) {
     $bot->handler(AskNameHandler::class)
         ->handler(function (TeleBot $bot, Objects\Update $update, callable $next) {
             if ($update->message()->text !== '/test' ?? null) {
@@ -64,10 +64,28 @@ test('request input handler', function (TeleBot $bot) {
         ]
     ]);
 
+    $wrongUserUpdate = Objects\Update::from([
+        'update_id' => 2,
+        'message' => [
+            'message_id' => 5,
+            'text' => 'Alan',
+            'from' => [
+                'id' => 2,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+            'date' => 2,
+        ]
+    ]);
+
     $correctNameUpdate = Objects\Update::from([
         'update_id' => 3,
         'message' => [
-            'message_id' => 5,
+            'message_id' => 6,
             'text' => 'John',
             'from' => [
                 'id' => 1,
@@ -119,6 +137,415 @@ test('request input handler', function (TeleBot $bot) {
     $res = $bot->handle($wrongNameUpdate);
     expect($res)->toBeInstanceOf(Objects\Message::class);
     expect($res->text)->toBe('Sorry, I don\'t know you.');
+
+    $res = $bot->handle($wrongUserUpdate);
+    expect($res)->toBeNull();
+
+    $res = $bot->handle($correctNameUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Hello, John!');
+})->with('bots');
+
+test('request input handler by chat', function (TeleBot $bot) {
+    $bot->handler(AskNameHandler::class)
+        ->handler(function (TeleBot $bot, Objects\Update $update, callable $next) {
+            if ($update->message()->text !== '/test' ?? null) {
+                return $next();
+            }
+
+            AskNameHandler::request($bot, $update->chat());
+
+            return $bot->sendMessage(
+                chat_id: $update->chat()->id,
+                text: 'Please enter your name.',
+            );
+        });
+
+    $initialUpdate = Objects\Update::from([
+        'update_id' => 1,
+        'message' => [
+            'message_id' => 1,
+            'text' => '/test',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'date' => 1,
+            'entities' => [
+                [
+                    'type' => 'bot_command',
+                    'offset' => 0,
+                    'length' => 5
+                ]
+            ]
+        ],
+    ]);
+
+    $wrongNameUpdate = Objects\Update::from([
+        'update_id' => 2,
+        'message' => [
+            'message_id' => 3,
+            'text' => 'Alan',
+            'from' => [
+                'id' => 2,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'date' => 2,
+        ]
+    ]);
+
+    $correctNameUpdate = Objects\Update::from([
+        'update_id' => 3,
+        'message' => [
+            'message_id' => 5,
+            'text' => 'John',
+            'from' => [
+                'id' => 3,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'date' => 4,
+        ]
+    ]);
+
+    $bot->fake([
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 2,
+            'date' => 1,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+            'text' => 'Please enter your name.',
+        ])),
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 4,
+            'date' => 2,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+            'text' => 'Sorry, I don\'t know you.',
+        ])),
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 6,
+            'date' => 4,
+            'chat' => [
+                'id' => 1,
+                'type' => 'private',
+            ],
+            'text' => 'Hello, John!',
+        ])),
+    ]);
+
+    $res = $bot->handle($initialUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Please enter your name.');
+
+    $res = $bot->handle($wrongNameUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Sorry, I don\'t know you.');
+
+    $res = $bot->handle($correctNameUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Hello, John!');
+})->with('bots');
+
+test('request input handler by user and chat', function (TeleBot $bot) {
+    $bot->handler(AskNameHandler::class)
+        ->handler(function (TeleBot $bot, Objects\Update $update, callable $next) {
+            if ($update->message()->text !== '/test' ?? null) {
+                return $next();
+            }
+
+            AskNameHandler::request($bot, $update->chat(), $update->user());
+
+            return $bot->sendMessage(
+                chat_id: $update->chat()->id,
+                text: 'Please enter your name.',
+            );
+        });
+
+    $initialUpdate = Objects\Update::from([
+        'update_id' => 1,
+        'message' => [
+            'message_id' => 1,
+            'text' => '/test',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'date' => 1,
+            'entities' => [
+                [
+                    'type' => 'bot_command',
+                    'offset' => 0,
+                    'length' => 5
+                ]
+            ]
+        ],
+    ]);
+
+    $wrongNameUpdate = Objects\Update::from([
+        'update_id' => 2,
+        'message' => [
+            'message_id' => 3,
+            'text' => 'Alan',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'date' => 2,
+        ]
+    ]);
+
+    $wrongChatUpdate = Objects\Update::from([
+        'update_id' => 3,
+        'message' => [
+            'message_id' => 4,
+            'text' => 'Alan',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 2,
+                'type' => 'group',
+            ],
+            'date' => 3,
+        ]
+    ]);
+
+    $correctNameUpdate = Objects\Update::from([
+        'update_id' => 4,
+        'message' => [
+            'message_id' => 5,
+            'text' => 'John',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'date' => 4,
+        ]
+    ]);
+
+    $bot->fake([
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 2,
+            'date' => 1,
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'text' => 'Please enter your name.',
+        ])),
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 4,
+            'date' => 2,
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'text' => 'Sorry, I don\'t know you.',
+        ])),
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 6,
+            'date' => 4,
+            'chat' => [
+                'id' => 1,
+                'type' => 'group',
+            ],
+            'text' => 'Hello, John!',
+        ])),
+    ]);
+
+    $res = $bot->handle($initialUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Please enter your name.');
+
+    $res = $bot->handle($wrongNameUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Sorry, I don\'t know you.');
+
+    $res = $bot->handle($wrongChatUpdate);
+    expect($res)->toBeNull();
+
+    $res = $bot->handle($correctNameUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Hello, John!');
+})->with('bots');
+
+test('request input handler by message thread', function (TeleBot $bot) {
+    $bot->handler(AskNameHandler::class)
+        ->handler(function (TeleBot $bot, Objects\Update $update, callable $next) {
+            if ($update->message()->text !== '/test' ?? null) {
+                return $next();
+            }
+
+            AskNameHandler::request($bot, $update->user(), message_thread_id: $update->message->message_thread_id);
+
+            return $bot->sendMessage(
+                chat_id: $update->chat()->id,
+                text: 'Please enter your name.',
+            );
+        });
+
+    $initialUpdate = Objects\Update::from([
+        'update_id' => 1,
+        'message' => [
+            'message_id' => 1,
+            'message_thread_id' => 1,
+            'text' => '/test',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'date' => 1,
+            'entities' => [
+                [
+                    'type' => 'bot_command',
+                    'offset' => 0,
+                    'length' => 5
+                ]
+            ]
+        ],
+    ]);
+
+    $wrongNameUpdate = Objects\Update::from([
+        'update_id' => 2,
+        'message' => [
+            'message_id' => 3,
+            'message_thread_id' => 1,
+            'text' => 'Alan',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'date' => 2,
+        ]
+    ]);
+
+    $wrongChatUpdate = Objects\Update::from([
+        'update_id' => 3,
+        'message' => [
+            'message_id' => 3,
+            'message_thread_id' => 2,
+            'text' => 'Alan',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'date' => 2,
+        ]
+    ]);
+
+    $correctNameUpdate = Objects\Update::from([
+        'update_id' => 4,
+        'message' => [
+            'message_id' => 5,
+            'message_thread_id' => 1,
+            'text' => 'John',
+            'from' => [
+                'id' => 1,
+                'is_bot' => false,
+                'first_name' => 'Alan',
+            ],
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'date' => 4,
+        ]
+    ]);
+
+    $bot->fake([
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 2,
+            'message_thread_id' => 1,
+            'date' => 1,
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'text' => 'Please enter your name.',
+        ])),
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 4,
+            'message_thread_id' => 1,
+            'date' => 2,
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'text' => 'Sorry, I don\'t know you.',
+        ])),
+        TeleBotResponse::make(Objects\Message::from([
+            'message_id' => 6,
+            'message_thread_id' => 1,
+            'date' => 4,
+            'chat' => [
+                'id' => 1,
+                'type' => 'supergroup',
+            ],
+            'text' => 'Hello, John!',
+        ])),
+    ]);
+
+    $res = $bot->handle($initialUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Please enter your name.');
+
+    $res = $bot->handle($wrongNameUpdate);
+    expect($res)->toBeInstanceOf(Objects\Message::class);
+    expect($res->text)->toBe('Sorry, I don\'t know you.');
+
+    $res = $bot->handle($wrongChatUpdate);
+    expect($res)->toBeNull();
 
     $res = $bot->handle($correctNameUpdate);
     expect($res)->toBeInstanceOf(Objects\Message::class);
