52dfb2008437b02f12225ce8f4e35877753850df
punyflash <14828695+punyflash@users.noreply.github.com>
2026-07-08 02:02:31 +0000

    [auto] update telegram bot API

--- src/Objects/InputPaidMediaLivePhoto.php
+++ src/Objects/InputPaidMediaLivePhoto.php
@@ -5,8 +5,8 @@ namespace WeStacks\TeleBot\Objects;
 /**
  * The paid media to send is a live photo.
  * @property-read string $type Type of the media, must be live_photo
- * @property-read string $media Video of the live photo to send. Pass a file_id to send a file that exists on the Telegram servers (recommended) or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name> name. More information on Sending Files ». Sending live photos by a URL is currently unsupported.
- * @property-read string $photo The static photo to send. Pass a file_id to send a file that exists on the Telegram servers (recommended) or pass "attach://<file_attach_name>" to upload a new one using multipart/form-data under <file_attach_name> name. More information on Sending Files ». Sending live photos by a URL is currently unsupported.
+ * @property-read string $media Video of the live photo to send. Pass a file_id to send a file that exists on the Telegram servers (recommended) or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. More information on Sending Files ». Sending live photos by a URL is currently unsupported.
+ * @property-read string $photo The static photo to send. Pass a file_id to send a file that exists on the Telegram servers (recommended) or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. More information on Sending Files ». Sending live photos by a URL is currently unsupported.
  *
  * @see https://core.telegram.org/bots/api#inputpaidmedialivephoto
  */
--- src/Objects/UniqueGiftInfo.php
+++ src/Objects/UniqueGiftInfo.php
@@ -8,8 +8,8 @@ use WeStacks\TeleBot\Foundation\TelegramObject;
  * Describes a service message about a unique gift that was sent or received.
  * @property-read UniqueGift $gift Information about the gift
  * @property-read string $origin Origin of the gift. Currently, either “upgrade” for gifts upgraded from regular gifts, “transfer” for gifts transferred from other users or channels, “resale” for gifts bought from other users, “gifted_upgrade” for upgrades purchased after the gift was sent, or “offer” for gifts bought or sold through gift purchase offers.
- * @property-read ?string $last_resale_currency Optional. For gifts bought from other users, the currency in which the payment for the gift was done. Currently, one of “XTR” for Telegram Stars or “TON” for toncoins.
- * @property-read ?int $last_resale_amount Optional. For gifts bought from other users, the price paid for the gift in either Telegram Stars or nanotoncoins
+ * @property-read ?string $last_resale_currency Optional. For gifts bought from other users, the currency in which the payment for the gift was done. Currently, one of “XTR” for Telegram Stars or “TON” for TON grams.
+ * @property-read ?int $last_resale_amount Optional. For gifts bought from other users, the price paid for the gift in either Telegram Stars or nanograms
  * @property-read ?string $owned_gift_id Optional. Unique identifier of the received gift for the bot; only present for gifts received on behalf of business accounts
  * @property-read ?int $transfer_star_count Optional. Number of Telegram Stars that must be paid to transfer the gift; omitted if the bot cannot transfer the gift
  * @property-read ?int $next_transfer_date Optional. Point in time (Unix timestamp) when the gift can be transferred. If it is in the past, then the gift can be transferred now.
